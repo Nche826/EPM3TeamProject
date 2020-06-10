6ba1da390ecd058941395bd669e03187c1451eae
Administrator <Administrator@MSDN-SPECIAL>
2020-06-10 16:32:55 +0900

    [EPM3TeamProject] file: openTList.html 등등
    index.html
    
    detail: 화면구현, 컨트롤연결

--- epm/src/main/java/com/cafe24/epm/controller/OpenDeviceController.java
+++ epm/src/main/java/com/cafe24/epm/controller/OpenDeviceController.java
@@ -1,8 +1,46 @@
 package com.cafe24.epm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OpenDeviceController {
+
+	@GetMapping("/openList")
+	public String openList() {
+		return "open/openList";
+		
+	}
+	@GetMapping("/openTList")
+	public String openTList() {
+		return "open/openTList";
+		
+	}
+	
+	@GetMapping("/openDeviceInsert")
+	public String openDeviceInsert() {
+		
+		return "open/openDeviceInsert";
+		
+	}
+	@GetMapping("/openDeviceUpdate")
+	public String openDeviceUpdate() {
+		
+		return "open/openDeviceUpdate";
+		
+	}
+	@GetMapping("/openUsimInsert")
+	public String openUsimInsert() {
+		
+		return "open/openUsimInsert";
+		
+	}
+	@GetMapping("/openUsimUpdate")
+	public String openUsimUpdate() {
+		
+		return "open/openUsimUpdate";
+		
+	}
+	
 
 }
