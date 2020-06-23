67fd28745ac90dbce2dd7d857c5764edbbb686cd
kimsuzy <kimsuzy@naver.com>
2020-06-23 16:38:07 +0900

    [EPM3TeamProject] 김수지 6.23 오후4:37
    detail: 직원등록처리 완료

--- epm/src/main/java/com/cafe24/epm/service/StaffService.java
+++ epm/src/main/java/com/cafe24/epm/service/StaffService.java
@@ -1,16 +1,20 @@
 package com.cafe24.epm.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cafe24.epm.domain.Staff;
+import com.cafe24.epm.mapper.StaffMapper;
 
 @Service
 public class StaffService {
 	
+	@Autowired private StaffMapper staffMapper;
+	
 	//직원등록처리
 	public int addStaff(Staff staff) {
 		System.out.println("=====staff 등록 서비스 시작=====");
-		return 0;
+		return staffMapper.addStaff(staff);
 	}
 
 }
