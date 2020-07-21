a6731008a4ee7d706093fe76d960d80228ed0a99
ttt1200 <gyh1200@naver.com>
2020-07-21 11:18:53 +0900

    [EPM3TeamProject] 최영민 detail: domain,mapper,service 파일 추가

---
+++ epm/src/main/java/com/cafe24/epm/domain/Used.java
@@ -0,0 +1,469 @@
+package com.cafe24.epm.domain;
+
+public class Used {
+	
+	private String usedinCode;
+	private String usedinSerial;
+	private String usedinTelecom;
+	private String usedinDevice;
+	private String usedinStatus;
+	private String usedinDate;
+	private String usedinPrice;
+	private String usedinStaff;
+	private String usedinMemo;
+	private String usedinRegDate;
+	
+	private String deviceCode;
+	private String deviceType;
+	private String deviceManuf;
+	private String deviceModel;
+	private String deviceName;
+	private String deviceColor;
+	private String devicePrice;
+	private String memberId;
+	private String deviceRegDate;
+	private String deviceTelecom;
+	
+	private String storeName;
+	
+	private String infoCode;
+	private String infoDevicein;
+	private String infoMethod;
+	private String infoStore;
+	private String infoMemo;
+	private String infoRegDate;
+	
+	private String updateStore;
+	private String updateStaff;
+	private String updateDate;
+	private String updateMethod;
+	private String updateDealler;
+	private String updateModel;
+	private String updateType;
+	private String updateColor;
+	private String updatePrice;
+	private String updateSerial;
+	private String updateMemo;
+	private String updateinCode;
+	private String updateTelecom;
+	
+	
+	private String deleteCode;
+	
+	private String searchDateStart;
+	private String searchDateEnd;
+	private String searchStatus;
+	private String searchDealler;
+	private String searchStaff;
+	private String searchSerial;
+	
+	private String deleteSerial;
+
+	public String getUsedinCode() {
+		return usedinCode;
+	}
+
+	public void setUsedinCode(String usedinCode) {
+		this.usedinCode = usedinCode;
+	}
+
+	public String getUsedinSerial() {
+		return usedinSerial;
+	}
+
+	public void setUsedinSerial(String usedinSerial) {
+		this.usedinSerial = usedinSerial;
+	}
+
+	public String getUsedinTelecom() {
+		return usedinTelecom;
+	}
+
+	public void setUsedinTelecom(String usedinTelecom) {
+		this.usedinTelecom = usedinTelecom;
+	}
+
+	public String getUsedinDevice() {
+		return usedinDevice;
+	}
+
+	public void setUsedinDevice(String usedinDevice) {
+		this.usedinDevice = usedinDevice;
+	}
+
+	public String getUsedinStatus() {
+		return usedinStatus;
+	}
+
+	public void setUsedinStatus(String usedinStatus) {
+		this.usedinStatus = usedinStatus;
+	}
+
+	public String getUsedinDate() {
+		return usedinDate;
+	}
+
+	public void setUsedinDate(String usedinDate) {
+		this.usedinDate = usedinDate;
+	}
+
+	public String getUsedinPrice() {
+		return usedinPrice;
+	}
+
+	public void setUsedinPrice(String usedinPrice) {
+		this.usedinPrice = usedinPrice;
+	}
+
+	public String getUsedinStaff() {
+		return usedinStaff;
+	}
+
+	public void setUsedinStaff(String usedinStaff) {
+		this.usedinStaff = usedinStaff;
+	}
+
+	public String getUsedinMemo() {
+		return usedinMemo;
+	}
+
+	public void setUsedinMemo(String usedinMemo) {
+		this.usedinMemo = usedinMemo;
+	}
+
+	public String getUsedinRegDate() {
+		return usedinRegDate;
+	}
+
+	public void setUsedinRegDate(String usedinRegDate) {
+		this.usedinRegDate = usedinRegDate;
+	}
+
+	public String getDeviceCode() {
+		return deviceCode;
+	}
+
+	public void setDeviceCode(String deviceCode) {
+		this.deviceCode = deviceCode;
+	}
+
+	public String getDeviceType() {
+		return deviceType;
+	}
+
+	public void setDeviceType(String deviceType) {
+		this.deviceType = deviceType;
+	}
+
+	public String getDeviceManuf() {
+		return deviceManuf;
+	}
+
+	public void setDeviceManuf(String deviceManuf) {
+		this.deviceManuf = deviceManuf;
+	}
+
+	public String getDeviceModel() {
+		return deviceModel;
+	}
+
+	public void setDeviceModel(String deviceModel) {
+		this.deviceModel = deviceModel;
+	}
+
+	public String getDeviceName() {
+		return deviceName;
+	}
+
+	public void setDeviceName(String deviceName) {
+		this.deviceName = deviceName;
+	}
+
+	public String getDeviceColor() {
+		return deviceColor;
+	}
+
+	public void setDeviceColor(String deviceColor) {
+		this.deviceColor = deviceColor;
+	}
+
+	public String getDevicePrice() {
+		return devicePrice;
+	}
+
+	public void setDevicePrice(String devicePrice) {
+		this.devicePrice = devicePrice;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getDeviceRegDate() {
+		return deviceRegDate;
+	}
+
+	public void setDeviceRegDate(String deviceRegDate) {
+		this.deviceRegDate = deviceRegDate;
+	}
+
+	public String getDeviceTelecom() {
+		return deviceTelecom;
+	}
+
+	public void setDeviceTelecom(String deviceTelecom) {
+		this.deviceTelecom = deviceTelecom;
+	}
+
+	public String getStoreName() {
+		return storeName;
+	}
+
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+
+	public String getInfoCode() {
+		return infoCode;
+	}
+
+	public void setInfoCode(String infoCode) {
+		this.infoCode = infoCode;
+	}
+
+	public String getInfoDevicein() {
+		return infoDevicein;
+	}
+
+	public void setInfoDevicein(String infoDevicein) {
+		this.infoDevicein = infoDevicein;
+	}
+
+	public String getInfoMethod() {
+		return infoMethod;
+	}
+
+	public void setInfoMethod(String infoMethod) {
+		this.infoMethod = infoMethod;
+	}
+
+	public String getInfoStore() {
+		return infoStore;
+	}
+
+	public void setInfoStore(String infoStore) {
+		this.infoStore = infoStore;
+	}
+
+	public String getInfoMemo() {
+		return infoMemo;
+	}
+
+	public void setInfoMemo(String infoMemo) {
+		this.infoMemo = infoMemo;
+	}
+
+	public String getInfoRegDate() {
+		return infoRegDate;
+	}
+
+	public void setInfoRegDate(String infoRegDate) {
+		this.infoRegDate = infoRegDate;
+	}
+
+	public String getUpdateStore() {
+		return updateStore;
+	}
+
+	public void setUpdateStore(String updateStore) {
+		this.updateStore = updateStore;
+	}
+
+	public String getUpdateStaff() {
+		return updateStaff;
+	}
+
+	public void setUpdateStaff(String updateStaff) {
+		this.updateStaff = updateStaff;
+	}
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public String getUpdateMethod() {
+		return updateMethod;
+	}
+
+	public void setUpdateMethod(String updateMethod) {
+		this.updateMethod = updateMethod;
+	}
+
+	public String getUpdateDealler() {
+		return updateDealler;
+	}
+
+	public void setUpdateDealler(String updateDealler) {
+		this.updateDealler = updateDealler;
+	}
+
+	public String getUpdateModel() {
+		return updateModel;
+	}
+
+	public void setUpdateModel(String updateModel) {
+		this.updateModel = updateModel;
+	}
+
+	public String getUpdateType() {
+		return updateType;
+	}
+
+	public void setUpdateType(String updateType) {
+		this.updateType = updateType;
+	}
+
+	public String getUpdateColor() {
+		return updateColor;
+	}
+
+	public void setUpdateColor(String updateColor) {
+		this.updateColor = updateColor;
+	}
+
+	public String getUpdatePrice() {
+		return updatePrice;
+	}
+
+	public void setUpdatePrice(String updatePrice) {
+		this.updatePrice = updatePrice;
+	}
+
+	public String getUpdateSerial() {
+		return updateSerial;
+	}
+
+	public void setUpdateSerial(String updateSerial) {
+		this.updateSerial = updateSerial;
+	}
+
+	public String getUpdateMemo() {
+		return updateMemo;
+	}
+
+	public void setUpdateMemo(String updateMemo) {
+		this.updateMemo = updateMemo;
+	}
+
+	public String getUpdateinCode() {
+		return updateinCode;
+	}
+
+	public void setUpdateinCode(String updateinCode) {
+		this.updateinCode = updateinCode;
+	}
+
+	public String getUpdateTelecom() {
+		return updateTelecom;
+	}
+
+	public void setUpdateTelecom(String updateTelecom) {
+		this.updateTelecom = updateTelecom;
+	}
+
+	public String getDeleteCode() {
+		return deleteCode;
+	}
+
+	public void setDeleteCode(String deleteCode) {
+		this.deleteCode = deleteCode;
+	}
+
+	public String getSearchDateStart() {
+		return searchDateStart;
+	}
+
+	public void setSearchDateStart(String searchDateStart) {
+		this.searchDateStart = searchDateStart;
+	}
+
+	public String getSearchDateEnd() {
+		return searchDateEnd;
+	}
+
+	public void setSearchDateEnd(String searchDateEnd) {
+		this.searchDateEnd = searchDateEnd;
+	}
+
+	public String getSearchStatus() {
+		return searchStatus;
+	}
+
+	public void setSearchStatus(String searchStatus) {
+		this.searchStatus = searchStatus;
+	}
+
+	public String getSearchDealler() {
+		return searchDealler;
+	}
+
+	public void setSearchDealler(String searchDealler) {
+		this.searchDealler = searchDealler;
+	}
+
+	public String getSearchStaff() {
+		return searchStaff;
+	}
+
+	public void setSearchStaff(String searchStaff) {
+		this.searchStaff = searchStaff;
+	}
+
+	public String getSearchSerial() {
+		return searchSerial;
+	}
+
+	public void setSearchSerial(String searchSerial) {
+		this.searchSerial = searchSerial;
+	}
+
+	public String getDeleteSerial() {
+		return deleteSerial;
+	}
+
+	public void setDeleteSerial(String deleteSerial) {
+		this.deleteSerial = deleteSerial;
+	}
+
+	@Override
+	public String toString() {
+		return "Used [usedinCode=" + usedinCode + ", usedinSerial=" + usedinSerial + ", usedinTelecom=" + usedinTelecom
+				+ ", usedinDevice=" + usedinDevice + ", usedinStatus=" + usedinStatus + ", usedinDate=" + usedinDate
+				+ ", usedinPrice=" + usedinPrice + ", usedinStaff=" + usedinStaff + ", usedinMemo=" + usedinMemo
+				+ ", usedinRegDate=" + usedinRegDate + ", deviceCode=" + deviceCode + ", deviceType=" + deviceType
+				+ ", deviceManuf=" + deviceManuf + ", deviceModel=" + deviceModel + ", deviceName=" + deviceName
+				+ ", deviceColor=" + deviceColor + ", devicePrice=" + devicePrice + ", memberId=" + memberId
+				+ ", deviceRegDate=" + deviceRegDate + ", deviceTelecom=" + deviceTelecom + ", storeName=" + storeName
+				+ ", infoCode=" + infoCode + ", infoDevicein=" + infoDevicein + ", infoMethod=" + infoMethod
+				+ ", infoStore=" + infoStore + ", infoMemo=" + infoMemo + ", infoRegDate=" + infoRegDate
+				+ ", updateStore=" + updateStore + ", updateStaff=" + updateStaff + ", updateDate=" + updateDate
+				+ ", updateMethod=" + updateMethod + ", updateDealler=" + updateDealler + ", updateModel=" + updateModel
+				+ ", updateType=" + updateType + ", updateColor=" + updateColor + ", updatePrice=" + updatePrice
+				+ ", updateSerial=" + updateSerial + ", updateMemo=" + updateMemo + ", updateinCode=" + updateinCode
+				+ ", updateTelecom=" + updateTelecom + ", deleteCode=" + deleteCode + ", searchDateStart="
+				+ searchDateStart + ", searchDateEnd=" + searchDateEnd + ", searchStatus=" + searchStatus
+				+ ", searchDealler=" + searchDealler + ", searchStaff=" + searchStaff + ", searchSerial=" + searchSerial
+				+ ", deleteSerial=" + deleteSerial + "]";
+	}
+
+	
+
+}
